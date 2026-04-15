d50afe38f83837cc21c71085842420130a1cb7a4
Okonompl <dkuhtin718@gmail.com>
2026-04-15 16:20:02 +0500

    gistogrammTHREE

--- frontend/script.js
+++ frontend/script.js
@@ -24,7 +24,7 @@ new Chart(document.getElementById('pieChart'), {
         labels: ['Ростово-суздальское княжество', 'Новгородское княжество', 'Галицко-Волынское княжество'],
         datasets: [{
             label: 'Торговля регионов в процентах',
-            data: [20, 50, 30],
+            data: [2, 5, 3],
             backgroundColor: ['red', 'blue', 'green']
         }]
 },
@@ -37,3 +37,40 @@ options: {
     }
 }
 });
+new Chart(document.getElementById('bar2Chart'), {
+    type: 'bar',
+    data: {
+        labels: ['Ростово-суздальское княжество', 'Новгородское княжество', 'Галицко-Волынское княжество'],
+        datasets: [{
+            label: 'Социальная свобода', 
+            data: [3, 10, 5],
+            backgroundColor: ['red', 'blue', 'green']
+        }]
+    },
+options: {
+    plugins: {
+        title: {
+            display: true,
+            text: 'Экономика регионов' 
+        }
+    }
+}
+});
+new Chart(document.getElementById('pie2Chart'), {
+    type: 'pie',
+    data: {
+        labels: ['Ростово-суздальское княжество', 'Новгородское княжество', 'Галицко-Волынское княжество'],
+        datasets: [{
+            label: 'Отдельная мощь регионов',
+            data: [9, 5, 9],
+            backgroundColor: ['red', 'blue', 'green']
+        }]
+},
+options: {
+    plugins: {
+        title: {
+            display: true,
+            text: 'Отдельная мощь регионов'
+        }
+    }
+}});
